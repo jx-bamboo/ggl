e7db16bdb46b2f6d69604d97c73357908d256647
laijianxing <jx-bamboo@outlook.com>
2017-10-25 11:55:42 +0800

    update deploy.rb rm do :before_hook do

--- config/deploy.rb
+++ config/deploy.rb
@@ -90,8 +90,6 @@ desc "Deploys the current version to the server."
 task :deploy do
   # uncomment this line to make sure you pushed your local branch to the remote origin
   # invoke :'git:ensure_pushed'
-  to :before_hook do
-  end
   deploy do
     # Put things that will set up an empty directory into a fully set-up
     # instance of your project.
